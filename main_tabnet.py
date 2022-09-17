70081c192245ed58578f69465bfa8e86bda13fd8
NaokiEvelyn77 <77677505+NaokiEvelyn77@users.noreply.github.com>
2022-09-18 07:32:28 +0800

    Add files via upload
    main_tabnet_forest

--- main_tabnet.py
+++ main_tabnet.py
@@ -1,126 +1,91 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr  8 19:12:46 2021
-
-@author: tiantian05
-"""
-# import xgboost as xgb
-from sklearn.metrics import r2_score
+# =============================================================================
+# 导入相应的包
+# =============================================================================
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
-import torch
-import warnings
-warnings.filterwarnings('ignore')
+np.random.seed(0)
+from sklearn.metrics import mean_squared_error,mean_absolute_error,accuracy_score
+import pandas as pd
+import numpy as np
 from pytorch_tabnet.tab_model import TabNetRegressor
+from xgboost import XGBRegressor,XGBClassifier
+from lightgbm import LGBMRegressor,LGBMClassifier
+from sklearn.ensemble import RandomForestRegressor,RandomForestClassifier
+import os
+import torch
+import math
 
 #导入构造好的数据
-data = pd.read_csv('F:/restaurant2020/train_firm_mean.csv')
+train = pd.read_csv('./data/house.csv')   #
+target = 'Prices'
+train['Garage'] = train['Garage']-1
+train['Baths'] = train['Baths']-1
+train['City'] = train['City']-1
 # =============================================================================
-# 新加的生成数据
+# 划分数据集
 # =============================================================================
-#generate_data = pd.read_csv('F:/restaurant2020/tabnet2021/generate_population.csv')
-#generate_data.columns = data.columns
-#data = pd.concat([data,generate_data],axis=0,ignore_index=True)
+if "Set" not in train.columns:
+    train["Set"] = np.random.choice(["train", "valid", "test"], p =[.8, .1, .1], size=(train.shape[0],))
 
+train_indices = train[train.Set=="train"].index
+valid_indices = train[train.Set=="valid"].index
+test_indices = train[train.Set=="test"].index
 
-test = pd.read_csv('F:/restaurant2020/test_firm_mean.csv')
-#训练数据和标签
-y = data['firm']
-train_data = data.drop(['firm'],axis=1)
-#测试数据和标签
-test_y = test['firm']
-test_data = test.drop(['firm'],axis=1)
+# =============================================================================
+# 训练所使用的的features
+# =============================================================================
+categorical_columns = ['Garage','FirePlace','Baths','White Marble',
+                       'Black Marble','Indian Marbel','Floors','City','Solar',
+                       'Electric','Fiber','Glass Doors','Swiming Pool','Garden']
+unused_feat = ['Set']
+features = [ col for col in train.columns if col not in unused_feat+[target]]
+cat_idxs = []
+for i,ele in enumerate(train.columns):
+    if ele in categorical_columns:
+        cat_idxs.append(i)
+cat_dims = [train[f].nunique() for i,f in enumerate(features) if f in categorical_columns]
+cat_emb_dim = 3
 
-#构建模型
+X_train = train[features].values[train_indices]
+y_train = train[target].values[train_indices].reshape(-1, 1)
+
+X_valid = train[features].values[valid_indices]
+y_valid = train[target].values[valid_indices].reshape(-1, 1)
+
+X_test = train[features].values[test_indices]
+y_test = train[target].values[test_indices].reshape(-1, 1)# 构建模型
+# =============================================================================
 clf = TabNetRegressor(
-        n_d = 32,
-        n_a = 32,
-        n_steps = 4,
-        gamma = 1.3,
-        n_shared = 1,
-        n_independent = 2,
-        seed = 2020,
-        lambda_sparse = 1e-3,
-        cat_dims=[], cat_emb_dim=[], cat_idxs=[])
-#损失函数
-cetr = torch.nn.L1Loss()
-#验证集的预测值数组
-oof_tab = np.zeros(len(train_data))
-#测试集的预测值
-prediction_tab = np.zeros(len(test_data))
+    n_d=64, n_a = 64, n_steps=5,
+    gamma=1.2, n_independent=2, n_shared=2,
+    cat_idxs=cat_idxs,cat_dims=cat_dims,cat_emb_dim=cat_emb_dim,
+    lambda_sparse=1e-4, momentum=0.2, clip_value=2.,
+    optimizer_fn= torch.optim.Adam,
+    optimizer_params=dict(lr=0.005),
+    scheduler_params = {"gamma": 0.1,
+                        "milestones":[4,10,13]},
+    scheduler_fn = torch.optim.lr_scheduler.MultiStepLR, epsilon=1e-15
+)
 
-trn_data, val_data, trn_y, val_y = train_test_split(train_data, y, test_size=0.2, random_state=42)
-trn_data = trn_data.values
-trn_y = trn_y.values.reshape(-1,1)
-val_data = val_data.values
-val_y = val_y.values.reshape(-1,1)
-clf.fit(trn_data,trn_y,
-        eval_set = [(val_data,val_y)],
-        eval_metric = ['mae'],
-        max_epochs = 150,
-        loss_fn = cetr,
-        patience = 50,
-        batch_size = 256, 
-        virtual_batch_size = 128,
-        num_workers = 0,
-        drop_last = False
-        )
-clf.f.close()
+clf.fit(
+    X_train=X_train, y_train=y_train,
+    eval_set=[(X_valid, y_valid)],
+    eval_name=['valid'],
+    eval_metric=['mae'],
+    max_epochs = 100, patience=100,
+    batch_size=512, virtual_batch_size=256
+)
 
-prediction_y = clf.predict(test_data.values)
-#employment
-#r2_1000=r2_score(test_y[0:317],prediction_y[0:317])
-#r2_1500=r2_score(test_y[317:530],prediction_y[317:530])
-#r2_2000=r2_score(test_y[530:686],prediction_y[530:686])
-#r2_2500=r2_score(test_y[686:807],prediction_y[686:807])
-#r2_3000=r2_score(test_y[807:1195],prediction_y[807:1195])
-#r2_3500=r2_score(test_y[1195:1275],prediction_y[1195:1275])
-#r2_4000=r2_score(test_y[1275:1343],prediction_y[1275:1343])
-#r2_4500=r2_score(test_y[1343:1400],prediction_y[1343:1400])
-#r2_5000=r2_score(test_y[1400:],prediction_y[1400:])
+preds = clf.predict(X_test)
+y_true = y_test
+test_score = mean_absolute_error(y_pred=preds, y_true=y_true)
+test_score_mse = mean_squared_error(y_pred=preds,y_true=y_true)
+test_score_rmse = math.sqrt(test_score_mse)
 
-
-##consumption
-#r2_1000=r2_score(test_y[0:256],prediction_y[0:256])
-#r2_1500=r2_score(test_y[256:437],prediction_y[256:437])
-#r2_2000=r2_score(test_y[437:574],prediction_y[437:574])
-#r2_2500=r2_score(test_y[574:683],prediction_y[574:683])
-#r2_3000=r2_score(test_y[683:771],prediction_y[683:771])
-#r2_3500=r2_score(test_y[771:844],prediction_y[771:844])
-#r2_4000=r2_score(test_y[844:906],prediction_y[844:906])
-#r2_4500=r2_score(test_y[906:959],prediction_y[906:959])
-#r2_5000=r2_score(test_y[959:],prediction_y[959:])
-
-##firm
-r2_1000=r2_score(test_y[0:305],prediction_y[0:305])
-r2_1500=r2_score(test_y[305:514],prediction_y[305:514])
-r2_2000=r2_score(test_y[514:667],prediction_y[514:667])
-r2_2500=r2_score(test_y[667:787],prediction_y[667:787])
-r2_3000=r2_score(test_y[787:885],prediction_y[787:885])
-r2_3500=r2_score(test_y[885:965],prediction_y[885:965])
-r2_4000=r2_score(test_y[965:1032],prediction_y[965:1032])
-r2_4500=r2_score(test_y[1032:1089],prediction_y[1032:1089])
-r2_5000=r2_score(test_y[1089:],prediction_y[1089:])
-
-
-#population
-#r2_1000=r2_score(test_y[0:315],prediction_y[0:315])
-#r2_1500=r2_score(test_y[315:527],prediction_y[315:527])
-#r2_2000=r2_score(test_y[527:682],prediction_y[527:682])
-#r2_2500=r2_score(test_y[682:803],prediction_y[682:803])
-#r2_3000=r2_score(test_y[803:1192],prediction_y[803:1192])
-#r2_3500=r2_score(test_y[1192:1272],prediction_y[1192:1272])
-#r2_4000=r2_score(test_y[1272:1339],prediction_y[1272:1339])
-#r2_4500=r2_score(test_y[1339:1396],prediction_y[1339:1396])
-#r2_5000=r2_score(test_y[1396:],prediction_y[1396:])
-
-print('网格id为1000的拟合系数为{:.4f}'.format(r2_1000))
-print('网格id为1500的拟合系数为{:.4f}'.format(r2_1500))
-print('网格id为2000的拟合系数为{:.4f}'.format(r2_2000))
-print('网格id为2500的拟合系数为{:.4f}'.format(r2_2500))
-print('网格id为3000的拟合系数为{:.4f}'.format(r2_3000))
-print('网格id为3500的拟合系数为{:.4f}'.format(r2_3500))
-print('网格id为4000的拟合系数为{:.4f}'.format(r2_4000))
-print('网格id为4500的拟合系数为{:.4f}'.format(r2_4500))
-print('网格id为5000的拟合系数为{:.4f}'.format(r2_5000))
+print(f"BEST VALID SCORE : {clf.best_cost}")
+print(f"FINAL TEST MAE SCORE FOR : {test_score}")
+print(f"FINAL TEST MSE SCORE FOR : {test_score_mse}")
+print(f"FINAL TEST RMSE SCORE FOR : {test_score_rmse}")
